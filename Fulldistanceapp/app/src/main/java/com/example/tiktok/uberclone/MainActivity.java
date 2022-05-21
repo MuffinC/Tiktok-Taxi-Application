b006e6a7f797fc6936dc831dcf8a643536a829b4
Mervyn_CJR <56317821+MuffinC@users.noreply.github.com>
2022-05-22 01:55:52 +0800

    LocationServices
    LocationServices request added

--- Fulldistanceapp/app/src/main/java/com/example/tiktok/uberclone/MainActivity.java
+++ Fulldistanceapp/app/src/main/java/com/example/tiktok/uberclone/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.tiktok.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
     private Button mDriver, mCustomer;
-
+    boolean isPermissionGranted; // by defualt it is false
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         mDriver = (Button) findViewById(R.id.driver);
         mCustomer= (Button) findViewById(R.id.customer);
+
+        checkPermission();
 
         mDriver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +55,30 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    private void checkPermission(){
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                isPermissionGranted =true;
+                Toast.makeText(MainActivity.this, "Permission Granted",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                //this will send the user to the setting of the application
+                Intent intent= new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri= Uri.fromParts("package",getPackageName(),"");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
     }
 }
