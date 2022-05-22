5dc9aa95291caa5688a8127f99ec3cb046c2bdda
Mervyn_CJR <56317821+MuffinC@users.noreply.github.com>
2022-05-22 13:48:55 +0800

    loc
    location snapping

--- Fulldistanceapp/app/src/main/java/com/example/tiktok/uberclone/DriverMapsActivity.java
+++ Fulldistanceapp/app/src/main/java/com/example/tiktok/uberclone/DriverMapsActivity.java
@@ -1,13 +1,18 @@
 package com.example.tiktok.uberclone;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.tiktok.uberclone.databinding.ActivityDriverMapsBinding;
@@ -36,7 +41,7 @@ public class DriverMapsActivity extends FragmentActivity implements OnMapReadyCa
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near Sydney, Australia.day2
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -45,10 +50,41 @@ public class DriverMapsActivity extends FragmentActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         //when the map is ready for use
+        LatLng latlng = new LatLng(1.340165306, 103.675497298);
+        //^will be the user's current position
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("My Current Location");
+        markerOptions.position(latlng);
+        mMap.addMarker(markerOptions);
+
+        //Camera update
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 13f);
+        mMap.animateCamera(cameraUpdate);
+
+        //Adding zoom controls
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        //Adding compass
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setScrollGesturesEnabled(true);
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
+
+        //My Location button
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+
+
+
     }
 }
