f74c394788e4df1b3553edbb1fe507eb3ccbd29b
Mervyn_CJR <56317821+MuffinC@users.noreply.github.com>
2022-05-28 15:46:15 +0800

    nav
    navigation drawer added to main page

--- Fulldistanceapp/app/src/main/java/com/example/tiktok/uberclone/MainActivity.java
+++ Fulldistanceapp/app/src/main/java/com/example/tiktok/uberclone/MainActivity.java
@@ -18,35 +18,79 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
 import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
     private Button mDriver, mCustomer;
     boolean isPermissionGranted; // by defualt it is false
 
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //mDriver = (Button) findViewById(R.id.driver);
+        setUpToolbar();
         mCustomer= (Button) findViewById(R.id.customer);
 
         checkPermission();
 
-
-        mCustomer.setOnClickListener(new View.OnClickListener() {
+        navigationView = (NavigationView) findViewById(R.id.navigation_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,CustomerLoginActivity.class);
-                startActivity(intent);
-                finish();
-                return;
-            }
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case  R.id.nav_home:
 
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case  R.id.customer:
+
+                        Intent intent2= new Intent(MainActivity.this, CustomerLoginActivity.class);
+                        startActivity(intent2);
+                        break;
+
+
+                    case  R.id.nav_share:{
+
+                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        sharingIntent.setType("text/plain");
+                        String shareBody =  "http://play.google.com/store/apps/detail?id=" + getPackageName();
+                        String shareSub = "Try now";
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                        startActivity(Intent.createChooser(sharingIntent, "Share using"));
+
+                    }
+                    break;
+                }
+                return false;
+            }
         });
     }
+    public void setUpToolbar() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
 
+    }
     private void checkPermission(){
         Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
             @Override
